c9a7ed4f308996fbb9d4cbeb1c90493049d6091f
a20220160 <a20220160@juan23.edu.pe>
2024-11-04 13:26:54 -0500

    Add files via upload

---
+++ operaciones (1).js
@@ -0,0 +1,25 @@
+document.write("<h1> Operaciones Basicas</h1>");
+
+var num1 = parseInt(prompt("Ingresa primer número"));
+var num2 = parseInt(prompt("Ingresa segundo número"));
+
+var sum = num1 + num2;
+var res = num1 - num2;
+var mul = num1 * num2;
+var div = num1 / num2;
+
+document.write("El primer número fue: " + num1);
+document.write("<br>");
+document.write("El segundo número fue: " + num2);
+document.write("<br>");
+document.write("<br>");
+document.write("la suma de ambos es: " + sum);
+document.write("<br>");
+document.write("la resta de ambos es: " + res);
+document.write("<br>");
+document.write("El producto de ambos es: " + mul);
+document.write("<br>");
+document.write("la división de ambos es: " + div);
+document.write("<br>");
+document.write("<br>");
+document.write("<marquee><h2>hecho por luciano wong</h2></marquee>");
